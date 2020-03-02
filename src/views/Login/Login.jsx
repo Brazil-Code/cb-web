f7e6aececf9e276fae54503db698e55897734e22
Andrew <andrew.jpb@outlook.com>
2020-03-02 13:33:34 -0300

    Integracao com api de Login

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,60 +1,72 @@
-import React, { Component } from 'react'
-import Axios from 'axios'
-import './style.css';
+import React, { Component } from "react";
+import Axios from "axios";
+import "./style.css";
 
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "",
+      password: ""
+    };
 
-class Login extends Component{
+    this.login = this.login.bind(this);
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            
-        }
+  login(e) {
+    e.preventDefault();
 
-        this.login = this.login.bind(this)
-    }
+    window.location.href = "/admin/dashboard";
+  }
 
-    login(e){
-        e.preventDefault();
-        window.location.href = "/admin/dashboard";
-    }
-
-    render(){
-        return(
-            <>
-                <div id="back">
-                    <canvas id="canvas" class="canvas-back"></canvas>
-                    <div class="backRight">    
-                    </div>
-                    <div class="backLeft">
-                        {/*Imagem aqui*/}
-                    </div>
-                </div>
-                <div id="slideBox">
-                    <div class="topLayer">
-                        <div class="right">
-                            <div class="content">
-                                <h2>Clean Budget</h2>
-                                <form id="form-login">
-                                <div class="form-element form-stack">
-                                    <label for="username-login" class="form-label">Usuario</label>
-                                    <input id="username-login" type="text" name="username"/>
-                                </div>
-                                <div class="form-element form-stack">
-                                    <label for="password-login" class="form-label">Senha</label>
-                                    <input id="password-login" type="password" name="password"/>
-                                </div>
-                                <div class="form-element form-submit">
-                                    <button id="logIn" class="login" onClick={this.login} name="login">Entrar</button>
-                            </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <div id="back">
+          <canvas id="canvas" class="canvas-back"></canvas>
+          <div class="backRight"></div>
+          <div class="backLeft">{/*Imagem aqui*/}</div>
+        </div>
+        <div id="slideBox">
+          <div class="topLayer">
+            <div class="right">
+              <div class="content">
+                <h2>Clean Budget</h2>
+                <form id="form-login">
+                  <div class="form-element form-stack">
+                    <label for="username-login" class="form-label">
+                      Usuario
+                    </label>
+                    <input id="username-login" type="text" name="username" />
+                  </div>
+                  <div class="form-element form-stack">
+                    <label for="password-login" class="form-label">
+                      Senha
+                    </label>
+                    <input
+                      id="password-login"
+                      type="password"
+                      name="password"
+                    />
+                  </div>
+                  <div class="form-element form-submit">
+                    <button
+                      id="logIn"
+                      class="login"
+                      onClick={this.login}
+                      name="login"
+                    >
+                      Entrar
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Login;
