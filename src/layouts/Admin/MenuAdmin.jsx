d10c102d5ae1bdc4dd2d97e32f0ac7ce56ed2b9c
Andrew <andrew.jpb@outlook.com>
2020-03-03 22:19:45 -0300

    BugFix
    Tela Inicial e Painel

---
+++ src/layouts/Admin/MenuAdmin.jsx
@@ -0,0 +1,16 @@
+let menu = [
+  {
+    path: "/dashboard", //url sem o admin
+    name: "Dashboard", //Nome no menu
+    icon: "tim-icons icon-chart-pie-36", //icone
+    layout: "/admin" //tipo de layout padrao adm
+  },
+  {
+    path: "/user-profile",
+    name: "User Profile",
+    icon: "tim-icons icon-single-02",
+    layout: "/admin"
+  }
+];
+
+export default menu;
