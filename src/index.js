0f611e2a9c3ced9c9d159a145eb6172fd72adfd3
Andrew <andrew.jpb@outlook.com>
2020-03-03 22:15:29 -0300

    Bug FIx
    Tela Inicial

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-
 import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
 
 import AdminLayout from "layouts/Admin/Admin.jsx";
-import Login from "./views/Login/Login"
+import Login from "./views/Login/";
 import "assets/scss/black-dashboard-react.scss";
 import "assets/demo/demo.css";
 import "assets/css/nucleo-icons.css";
@@ -16,7 +15,7 @@ ReactDOM.render(
   <Router history={hist}>
     <Switch>
       <Route path="/admin" render={props => <AdminLayout {...props} />} />
-      <Route exact path="/login" render={prop => <Login {...prop}/>} />
+      <Route exact path="/login" render={prop => <Login {...prop} />} />
       <Redirect from="/" to="/login" />
     </Switch>
   </Router>,
